bf409d8a22aca752ba2989ed868ab1c21fab52f9
Rushirufuruu <vicente.zapatasilva@gmail.com>
2025-09-18 16:22:59 -0300

    carpeta .next eliminada del repo y gitignore actualizado

--- cleanandgarden/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/PVqZUixHTv+jWTu3rmbkJoPUrfrvR4/+au5NSYhgYk=\"\n}"
--- cleanandgarden/web/.next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__62d56219._.css",
-  "static/chunks/_699b9151._.js"
-],
-    source: "dynamic"
-});
