d094c417404894543c5388af5a80aa4faa17c8c7
Rushirufuru <vicente.zapatasilva@gmail.com>
2025-09-21 17:28:34 -0300

    Se cambió la forma de linkear las páginas, estaba con <a href=#> y dió problemas en vercel. Ahora se cambió a <Link href=/>

--- cleanandgarden/web/src/app/components/Navbar.tsx
+++ cleanandgarden/web/src/app/components/Navbar.tsx
@@ -1,29 +1,30 @@
-"use client";
 
-import { useState } from "react";
+"use client";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-  <div className="navbar shadow-md px-4 sticky top-0 z-50" style={{ backgroundColor: '#f5e9d7' }}>
+    <div className="navbar shadow-md px-4 sticky top-0 z-50" style={{ backgroundColor: '#f5e9d7' }}>
       {/* Logo a la izquierda */}
       <div className="navbar-start">
-        <a href="/" style={{ display: 'inline-block' }}>
-          <img src="/logo.png" alt="Logo Clean & Garden" className="h-12 w-auto" />
-        </a>
+        <Link href="/" style={{ display: 'inline-block' }}>
+          <Image src="/logo.png" alt="Logo Clean & Garden" width={48} height={48} className="h-12 w-auto" priority />
+        </Link>
       </div>
       {/* Menú desktop centrado */}
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 gap-1">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/about-us">Quienes Somos</a></li>
-          <li><a href="/our-services">Servicios</a></li>
-          <li><a href="#">Portafolio</a></li>
-          <li><a href="#">Agenda tu hora</a></li>
+          <li><Link href="/">Inicio</Link></li>
+          <li><Link href="/about-us">Quienes Somos</Link></li>
+          <li><Link href="/our-services">Servicios</Link></li>
+          <li><Link href="/portfolio">Portafolio</Link></li>
+          <li><Link href="/book-appointment">Agenda tu hora</Link></li>
         </ul>
       </div>
       {/* Botón de autenticación a la derecha en desktop */}
       <div className="navbar-end hidden lg:flex">
-    <a href="#" className="btn rounded-lg" style={{ backgroundColor: '#4a7e49', color: '#fff', border: 'none' }}>Regístrate o inicia sesión</a>
+        <a href="#" className="btn rounded-lg" style={{ backgroundColor: '#4a7e49', color: '#fff', border: 'none' }}>Regístrate o inicia sesión</a>
       </div>
       {/* Menú móvil DaisyUI */}
       <div className="lg:hidden navbar-end" style={{ backgroundColor: '#f5e9d7' }}>
@@ -36,11 +37,11 @@ export default function Navbar() {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52"
             style={{ backgroundColor: '#f5e9d7' }}
           >
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/about-us">Quienes Somos</a></li>
-            <li><a href="/our-services">Servicios</a></li>
-            <li><a href="#">Portafolio</a></li>
-            <li><a href="#">Agenda tu hora</a></li>
+            <li><Link href="/">Inicio</Link></li>
+            <li><Link href="/about-us">Quienes Somos</Link></li>
+            <li><Link href="/our-services">Servicios</Link></li>
+            <li><Link href="/portfolio">Portafolio</Link></li>
+            <li><Link href="/book-appointment">Agenda tu hora</Link></li>
             <li><a href="#" className="btn rounded-lg w-full" style={{ backgroundColor: '#4a7e49', color: '#fff', border: 'none' }}>Regístrate o inicia sesión</a></li>
           </ul>
         </div>
